4318a8021fc505e8239235d5f925864a0a298270
niartenyaw <amwayne90@gmail.com>
2020-06-15 04:44:27 -0700

    update data

--- src/components/Marker.jsx
+++ src/components/Marker.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default ({ data }) => {
   const style = {
     position: "absolute",
-    top: `${data.years_from_today}px`
+    left: `${data.years_from_today}px`
   };
   return (
     <div className="marker" style={style}>
