39feb7e24f99efb11230166338cf171d2039d872
niartenyaw <amwayne90@gmail.com>
2020-06-26 00:54:34 -0700

    Updates

---
+++ precache-manifest.761411024e951cb2e67bd653feebf399.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dec22117eece87fcadca7fc93bc1ef0",
+    "url": "/if-a-year-were-only-a-pixel/index.html"
+  },
+  {
+    "revision": "148d08aadd92fbdb1f4e",
+    "url": "/if-a-year-were-only-a-pixel/static/css/main.17b21b1b.chunk.css"
+  },
+  {
+    "revision": "42207297f226e9fe70f7",
+    "url": "/if-a-year-were-only-a-pixel/static/js/2.dd67da82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/if-a-year-were-only-a-pixel/static/js/2.dd67da82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "148d08aadd92fbdb1f4e",
+    "url": "/if-a-year-were-only-a-pixel/static/js/main.27cd1aa4.chunk.js"
+  },
+  {
+    "revision": "f71b7d1ddb0db8f9cbaf",
+    "url": "/if-a-year-were-only-a-pixel/static/js/runtime-main.4a527526.js"
+  }
+]);
